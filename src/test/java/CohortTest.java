44b74c25d3bc1bcf3c0463d1ca85b8ee1f8709eb
MrOffer <thomas.e.offer@gmail.com>
2019-12-16 15:57:42 -0600

    taking a break

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,6 +1,20 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class CohortTest {
+    Student will = new Student(1L, "William");
+    Student fez = new Student(2L, "Fez");
+    Cohort Deimos = new Cohort();
+
+    @Before
+    public void setup(){
+        Deimos.addStudent(will);
+        Deimos.addStudent(fez);
+        will.addGrade(95);
+        will.addGrade(100);
+        fez.addGrade(90);
+        fez.addGrade(100);
+    }
 
     @Test
     public void testAddStudent(){
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,42 +1,42 @@
 
 
+    import org.junit.Before;
     import org.junit.Test;
 
 import static org.junit.Assert.*;
 
     public class StudentTest {
+        Student will = new Student(1L, "William");
+
+
         @Test
         public void testCreateStudent(){
-            Student Fez = new Student(1L, "Fez");
-            Student will = null;
-            assertNull(will);
-            assertNotNull(Fez);
+            Student fez = null;
+            assertNull(fez);
+            assertNotNull(will);
         }
 
         @Test
         public void testStudents(){
-            Student fer = new Student(1L, "Fez");
-            assertSame(1L, fer.getId());
-            assertSame("Fez", fer.getName());
-            assertSame(0, fer.getGrades().size());
+            assertSame(1L, will.getId());
+            assertSame("William", will.getName());
+            assertSame(0, will.getGrades().size());
         }
 
 
         @Test
         public void testAddGrade(){
-            Student fez = new Student(1L, "Fez");
-            fez.addGrade(100);
-            assertSame(100, fez.getGrades().get(0));
-            fez.addGrade(80);
-            assertSame(80, fez.getGrades().get(1));
+            will.addGrade(100);
+            assertSame(100, will.getGrades().get(0));
+            will.addGrade(80);
+            assertSame(80, will.getGrades().get(1));
         }
 
         @Test
         public void testAverageGrade(){
-            Student fez = new Student(1L, "Fez");
-            fez.addGrade(100);
-            fez.addGrade(80);
-            assertEquals(90, fez.getGradeAverage(), 0);
+            will.addGrade(100);
+            will.addGrade(80);
+            assertEquals(90, will.getGradeAverage(), 0);
         }
     }
 
