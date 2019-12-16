64fed339afdae1b665bcc4105cb2503ba63f3342
MrOffer <thomas.e.offer@gmail.com>
2019-12-16 12:12:04 -0600

    finished

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,44 @@
+
+
+    import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+    public class StudentTest {
+        @Test
+        public void testCreateStudent(){
+            Student Fez = new Student(1L, "Fez");
+            Student will = null;
+            assertNull(will);
+            assertNotNull(Fez);
+        }
+
+        @Test
+        public void testStudentFields(){
+            Student fer = new Student(1L, "Fez");
+            assertSame(1L, fer.getId());
+            assertSame("Fez", fer.getName());
+            assertSame(0, fer.getGrades().size());
+        }
+
+
+        @Test
+        public void testAddGrade(){
+            Student fez = new Student(1L, "Fez");
+            fez.addGrade(100);
+            assertSame(100, fez.getGrades().get(0));
+            fez.addGrade(80);
+            assertSame(80, fez.getGrades().get(1));
+        }
+
+        @Test
+        public void testAverageGrade(){
+            Student fez = new Student(1L, "Fez");
+            fez.addGrade(100);
+            fez.addGrade(80);
+            assertEquals(90, fez.getGradeAverage(), 0);
+        }
+    }
+
