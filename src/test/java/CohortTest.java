3980c9ca13d5f1911eee4720fd63c1bc1b8e4526
Bryce Payne <brycedpayne@gmail.com>
2020-04-20 08:55:17 -0500

    tests

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -34,12 +34,33 @@ public class CohortTest {
     public void testAddStudentAndGetStudentsWork(){
         assertEquals(0, emptyCohort.getStudents().size());
         assertEquals(1, cohortWithOne.getStudents().size());
-//        assertEquals(1, cohortWithOne.getStudents().get(0).getId());
+        assertEquals(1, cohortWithOne.getStudents().get(0).getId());
     }
 
     @Test
     public void testIfAvgIsCorrect(){
         assertEquals(95.0, cohortWithMany.getCohortAverage(), 0);
+    }
+
+    @Test
+    public void testIfStudentListIsCorrect(){
+        assertTrue(emptyCohort.getStudents().isEmpty());
+        int counter = 0;
+        for (Student student : cohortWithOne.getStudents()) {
+            assertSame(student.getId(),cohortWithOne.getStudents().get(counter).getId());
+            assertSame(student.getName(),cohortWithOne.getStudents().get(counter).getName());
+            assertSame(student.getGrades(),cohortWithOne.getStudents().get(counter).getGrades());
+            assertEquals(student.getGradeAverage(),cohortWithOne.getStudents().get(counter).getGradeAverage(),0);
+            counter++;
+        }
+        counter = 0;
+        for (Student student : cohortWithMany.getStudents()) {
+            assertSame(student.getId(),cohortWithMany.getStudents().get(counter).getId());
+            assertSame(student.getName(),cohortWithMany.getStudents().get(counter).getName());
+            assertSame(student.getGrades(),cohortWithMany.getStudents().get(counter).getGrades());
+            assertEquals(student.getGradeAverage(),cohortWithMany.getStudents().get(counter).getGradeAverage(),0);
+            counter++;
+        }
     }
 
 }
